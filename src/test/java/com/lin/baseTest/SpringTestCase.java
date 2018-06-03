fa1c7796a3f0d0b41c0c58e43ff260304eb67477
wangbin <wangbin_dg@163.com>
2018-06-03 16:21:45 +0800

    test

--- src/test/java/com/lin/baseTest/SpringTestCase.java
+++ src/test/java/com/lin/baseTest/SpringTestCase.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+//gittestgit
+
 //指定bean注入的配置文件
 @ContextConfiguration(locations = { "classpath:application.xml" })
 // 使用标准的JUnit @RunWith注释来告诉JUnit使用的Spring TestRunner
